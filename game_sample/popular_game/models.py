239523140f2356e8d4ce37b6f54ee63ab8e17ac1
kentaro-tsutsumi <kentar0.2tsumi@gmail.com>
2016-10-13 13:45:04 +0900

    Baseモデル作成

--- game_sample/popular_game/models.py
+++ game_sample/popular_game/models.py
@@ -1,3 +1,13 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Base(models.Model):
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
