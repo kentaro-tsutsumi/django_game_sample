6ac94117032df930c771e90cf19370b90aa31f6a
kentaro-tsutsumi <kentar0.2tsumi@gmail.com>
2016-10-13 15:28:21 +0900

    Playerモデルにレベルの概念を追加

--- game_sample/popular_game/models.py
+++ game_sample/popular_game/models.py
@@ -15,6 +15,22 @@ class Base(models.Model):
 
 class Player(Base):
     name = models.CharField(max_length=10, db_index=True)
+    level = models.IntegerField(blank=False, default=1)
+    exp = models.BigIntegerField(default=0)
+    LEVEL_EXPS = [0, 100, 200, 300, 400, 500]
+
+    def __init__(self, *args, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+        setattr(self, 'initial_exp', self.exp)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.initial_exp != self.exp:
+            for i, level_exp in enumerate(self.LEVEL_EXPS):
+                if self.exp >= level_exp:
+                    self.level = i + 1
+                else:
+                    break
+        super(Player, self).save(*args, **kwargs)
